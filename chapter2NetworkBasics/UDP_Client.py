a3e4d0861f379b43da01ae6542991f0e3a111b5f
bucketmask <43216179+bucketmask@users.noreply.github.com>
2021-10-01 14:12:42 +0100

    Add files via upload

---
+++ chapter2NetworkBasics/UDP_Client.py
@@ -0,0 +1,14 @@
+import socket
+
+target_host = "127.0.0.1"
+target_port = 80
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+msg = "AAABBC"
+byte = msg.encode()
+client.sendto(byte, (target_host, target_port))
+
+data, addr = client.recvfrom(4096)
+
+print(data)
